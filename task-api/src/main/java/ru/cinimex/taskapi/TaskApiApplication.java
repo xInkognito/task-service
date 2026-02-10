c7ca203c65805f3b610500ae436c6cfebf70ca92
Inkognito <inkognita_03@mail.ru>
2026-02-10 14:16:32 +0500

    Init

---
+++ task-api/src/main/java/ru/cinimex/taskapi/TaskApiApplication.java
@@ -0,0 +1,13 @@
+package ru.cinimex.taskapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskApiApplication.class, args);
+    }
+
+}
